35add82729384802d30c0333b413a517999f8416
Leo Kosta <kostaleonard@gmail.com>
2023-05-25 09:42:42 -0400

    Added skill widget
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
 
 class RenaissanceManApp extends StatelessWidget {
   static const appTitle = 'Renaissance Man';
+  static const skills = ['Piano', 'Russian', 'Cooking', 'Coding']; // TODO this is test data
 
   const RenaissanceManApp({super.key});
 
@@ -17,9 +18,25 @@ class RenaissanceManApp extends StatelessWidget {
         title: 'Renaissance Man',
         theme: ThemeData(
           appBarTheme: const AppBarTheme(
-              backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+              backgroundColor: Colors.blueGrey, foregroundColor: Colors.white),
         ),
         home: Scaffold(
-            appBar: AppBar(title: const Text(appTitle)), body: const Column()));
+          appBar: AppBar(title: const Text(appTitle)),
+          body: ListView.builder(
+              itemCount: skills.length * 2,
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (context, i) {
+                if (i.isOdd) return const Divider();
+                final index = i ~/ 2;
+                return ListTile(
+                    leading: const Icon(Icons.bolt),
+                    title: Text(skills[index]));
+              }),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => (), // TODO add skill API call
+            tooltip: 'Add skill',
+            child: const Icon(Icons.add),
+          ),
+        ));
   }
 }
