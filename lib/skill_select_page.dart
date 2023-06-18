be78fe2aac344b428a3f74697b6b7fc019d4a518
Leo Kosta <kostaleonard@gmail.com>
2023-06-18 09:25:59 -0400

    Added create skill popup widget at correct position
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- lib/skill_select_page.dart
+++ lib/skill_select_page.dart
@@ -1,5 +1,7 @@
 /// Displays the user's tracked skills.
 
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:renaissance_man/skill.dart';
@@ -18,6 +20,7 @@ class SkillSelectPage extends StatefulWidget {
 
 class _SkillSelectPageState extends State<SkillSelectPage> {
   static const _biggerFont = TextStyle(fontSize: 18);
+  static const _gridViewMaxCrossAxisExtent = 200.0;
   late Future<List<Skill>> skillQuery;
 
   @override
@@ -37,7 +40,6 @@ class _SkillSelectPageState extends State<SkillSelectPage> {
         appBar: AppBar(title: const Text('Renaissance Man')),
         body: Column(
           children: [
-
             FutureBuilder(
               future: skillQuery,
               builder: (context, snapshot) {
@@ -62,13 +64,41 @@ class _SkillSelectPageState extends State<SkillSelectPage> {
                       },
                       child: const Icon(Icons.add));
                   return Expanded(
-                      child: GridView.extent(
-                    padding: const EdgeInsets.all(8),
-                    mainAxisSpacing: 25,
-                    crossAxisSpacing: 10,
-                    maxCrossAxisExtent: 200,
-                    children: [addSkillButton as Widget] + skillPreviewCards,
-                  ));
+                      child: LayoutBuilder(builder: (context, constraints) {
+                    //TODO update magic numbers to static constants
+                    //See Flutter's SliverGridDelegateWithMaxCrossAxisExtent for an explanation of this calculation.
+                    final numGridViewColumns = max(
+                        1,
+                        ((constraints.biggest.width - 16) /
+                                (_gridViewMaxCrossAxisExtent + 10))
+                            .ceil());
+                    final usableCrossAxisExtent = max(
+                      0,
+                      constraints.biggest.width -
+                          16 -
+                          10 * (numGridViewColumns - 1),
+                    );
+                    final gridViewColumnSize =
+                        usableCrossAxisExtent / numGridViewColumns;
+                    return Stack(children: [
+                      GridView.extent(
+                        padding: const EdgeInsets.all(8),
+                        mainAxisSpacing: 25,
+                        crossAxisSpacing: 10,
+                        maxCrossAxisExtent: _gridViewMaxCrossAxisExtent,
+                        children: <Widget>[addSkillButton] + skillPreviewCards,
+                      ),
+                      Positioned(
+                          top: 8,
+                          left: 8 + gridViewColumnSize,
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.red,
+                            child: Text('$numGridViewColumns'),
+                          ))
+                    ]);
+                  }));
                 }
               },
             ),
