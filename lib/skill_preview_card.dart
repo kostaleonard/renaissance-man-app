a4d84cb594301df1ba375199cf307f54cb7c01be
leonard.kosta <kostaleonard@gmail.com>
2023-07-09 15:45:20 -0400

    Added total practice time to skill preview card

--- lib/skill_preview_card.dart
+++ lib/skill_preview_card.dart
@@ -5,15 +5,31 @@ import 'package:flutter/material.dart';
 import 'package:renaissance_man/repository.dart';
 import 'package:renaissance_man/skill.dart';
 import 'package:renaissance_man/skill_page.dart';
+import 'package:renaissance_man/weekly_practice_schedule.dart';
 
 //TODO when a user closes the skill page after making changes, do those changes reflect on the card here?
-class SkillPreviewCard extends StatelessWidget {
+class SkillPreviewCard extends StatefulWidget {
   final Repository repository;
   final Skill skill;
   static const height = 298.0;
 
   const SkillPreviewCard(
       {super.key, required this.repository, required this.skill});
+
+  @override
+  State<StatefulWidget> createState() => _SkillPreviewCardState();
+}
+
+class _SkillPreviewCardState extends State<SkillPreviewCard> {
+  static const _biggerFont = TextStyle(fontSize: 18);
+  late Future<List<WeeklyPracticeSchedule>> readWeeklyPracticeScheduleQuery;
+
+  @override
+  void initState() {
+    super.initState();
+    readWeeklyPracticeScheduleQuery = widget.repository
+        .readWeeklyPracticeSchedules(widget.skill.weeklyPracticeScheduleIds);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,21 +40,60 @@ class SkillPreviewCard extends StatelessWidget {
         return Card(
           clipBehavior: Clip.antiAlias,
           child: InkWell(
-            onTap: openContainer,
-            splashColor:
-                Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
-            highlightColor: Colors.transparent,
-            child: Text(skill.name),
-            //TODO also display number of hours
-          ),
+              onTap: openContainer,
+              splashColor:
+                  Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
+              highlightColor: Colors.transparent,
+              child: Column(children: [
+                Text(widget.skill.name),
+                FutureBuilder(
+                    future: readWeeklyPracticeScheduleQuery,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                            child: Text('No connection', style: _biggerFont));
+                      } else {
+                        final weeklyPracticeSchedules = snapshot.data!;
+                        final now = DateTime.now();
+                        final today = DateTime(now.year, now.month, now.day);
+                        final weeklyPracticeSchedulesTotalDuration =
+                            weeklyPracticeSchedules.map((schedule) =>
+                                schedule.getTimePracticedBetween(
+                                    schedule.startRecurrence,
+                                    schedule.endRecurrence ?? today));
+                        final totalPracticeDuration = weeklyPracticeSchedules
+                                .isEmpty
+                            ? Duration.zero
+                            : weeklyPracticeSchedulesTotalDuration
+                                .reduce((value, element) => value + element);
+                        return Text(
+                            _getDurationDisplayString(totalPracticeDuration));
+                      }
+                    })
+              ])),
         );
       },
       openBuilder: (context, closeContainer) {
         return SkillPage(
-          repository: repository,
-          skillId: skill.id,
+          repository: widget.repository,
+          skillId: widget.skill.id,
         );
       },
     );
+  }
+
+  String _getDurationDisplayString(Duration duration) {
+    final hours = duration.inHours;
+    final String hoursSummary;
+    if (hours == 1) {
+      hoursSummary = '1 hour';
+    } else if (hours > 1000) {
+      hoursSummary = '${(hours / 1000).floor()}k hours';
+    } else {
+      hoursSummary = '$hours hours';
+    }
+    return hoursSummary;
   }
 }
