7cfa6277923ba0d244c5068a361cc77f387d59fe
leonard.kosta <kostaleonard@gmail.com>
2023-07-31 13:10:10 -0400

    Added basic login page UI

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:renaissance_man/routes.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,7 +11,68 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO implement build
-    return Scaffold();
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+            child: Align(
+                alignment: Alignment.center,
+                child: ListView(
+                  children: [
+                    _UsernameInput(),
+                    const SizedBox(height: 12),
+                    _PasswordInput(),
+                    _LoginButton(onTap: () {
+                      _login(context);
+                    })
+                  ],
+                )))
+      ],
+    ));
+  }
+
+  void _login(BuildContext context) {
+    Navigator.of(context).restorablePushNamed(homeRoute);
+  }
+}
+
+class _UsernameInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(labelText: 'Username'),
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(labelText: 'Password'),
+      obscureText: true,
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const _LoginButton({required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onTap,
+      child: const Row(
+        children: [
+          Icon(Icons.lock),
+          SizedBox(
+            width: 6,
+          ),
+          Text('Login')
+        ],
+      ),
+    );
   }
 }
